8a3f4afbc6d03cbfe1c40ba21c1ad80e27e120b4
Error404notafound <error404notafound@gmail.com>
2026-05-20 02:35:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-publish').addEventListener('click', () => {
+    alert('O botão do Blueberry funciona! Próximo passo: conectar o Firebase para salvar vídeos e mensagens.');
+});
